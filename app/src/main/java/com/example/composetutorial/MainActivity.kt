c7c8dc99e279aa0efedbb9ccda9918e4af02bc41
Rubi Ben-Zvi <rubi.benzvi@gmail.com>
2023-03-14 13:07:26 +0200

    rubi

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme.colors
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
@@ -24,11 +25,14 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Typeface
+import androidx.compose.ui.unit.sp
 import java.lang.Math.*
 import kotlin.math.atan2
 
@@ -57,114 +61,118 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Content() {
-    var radius by remember {mutableStateOf(0f)}
-    var shapeCenter by remember {mutableStateOf(Offset.Zero)}
-    var handleCenter by remember {mutableStateOf(Offset.Zero)}
-    var angle by remember {mutableStateOf(20.0)}
-    val imageModifier = Modifier
-        .offset(y= ((height/2)-rd/2).dp,x= ((width/2)-rd/2).dp)
-        .clip(CircleShape)
-        .size((rd).dp)
-
-
-
-    Canvas(
-        modifier = Modifier
-            .fillMaxSize()
-            .drawWithCache {
-                val brush = Brush.linearGradient(
-                    listOf(
-                        Color(0xFF9E82F0),
-                        Color(0xFF42A5F5)
+   // Column {
+     //   Text(text = "msg.author")
+        var radius by remember { mutableStateOf(0f) }
+        var shapeCenter by remember { mutableStateOf(Offset.Zero) }
+        var handleCenter by remember { mutableStateOf(Offset.Zero) }
+        var angle by remember { mutableStateOf(20.0) }
+        val imageModifier = Modifier
+            .offset(y = ((height / 2) - rd / 2).dp, x = ((width / 2) - rd / 2).dp)
+            .clip(CircleShape)
+            .size((rd).dp)
+        Canvas(
+            modifier = Modifier
+                .fillMaxSize()
+                .drawWithCache {
+                    val brush = Brush.linearGradient(
+                        listOf(
+                            Color(0xFF9E82F0),
+                            Color(0xFF42A5F5)
+                        )
                     )
-                )
-                onDrawBehind {
-                    drawRoundRect(
-                        brush,
-                        cornerRadius = CornerRadius(10.dp.toPx())
-                    )
+                    onDrawBehind {
+                        drawRoundRect(
+                            brush,
+                            cornerRadius = CornerRadius(10.dp.toPx())
+                        )
+                    }
                 }
-            }
-            .pointerInput(Unit) {
-                detectDragGestures { change, dragAmount ->
-                    handleCenter += dragAmount
-                    angle = getRotationAngle(handleCenter, shapeCenter)
-                    change.consume()
+                .pointerInput(Unit) {
+                    detectDragGestures { change, dragAmount ->
+                        handleCenter += dragAmount
+                        angle = getRotationAngle(handleCenter, shapeCenter)
+                        change.consume()
+                    }
                 }
-            }
-            .padding(100.dp)
-    ) {
-        shapeCenter = center
-        radius = size.minDimension / 2+100
-        val x = (shapeCenter.x + cos(toRadians(angle)) * radius).toFloat()
-        val y = (shapeCenter.y + sin(toRadians(angle)) * radius).toFloat()
-        handleCenter = Offset(x, y)
-
-        drawArc(
-            color = Color.Green,
-            startAngle = 270f,
-            sweepAngle = 72f,
-            useCenter = false,
-            style = Stroke(80f),
-            topLeft = Offset(shapeCenter.x-radius,shapeCenter.y-radius),
-            size = Size(radius*2, radius*2)
+                .padding(100.dp)
         )
 
-        drawArc(
-            color = Color.Cyan,
-            startAngle = 342f,
-            sweepAngle = 72f,
-            useCenter = false,
-            style = Stroke(80f),
-            topLeft = Offset(shapeCenter.x-radius,shapeCenter.y-radius),
-            size = Size(radius*2, radius*2)
-        )
-        drawArc(
-            color = Color.Yellow,
-            startAngle = 54f,
-            sweepAngle = 72f,
-            useCenter = false,
-            style = Stroke(80f),
-            topLeft = Offset(shapeCenter.x-radius,shapeCenter.y-radius),
-            size = Size(radius*2, radius*2)
-        )
-        drawArc(
-            color = Color.Magenta,
-            startAngle = 126f,
-            sweepAngle = 72f,
-            useCenter = false,
-            style = Stroke(80f),
-            topLeft = Offset(shapeCenter.x-radius,shapeCenter.y-radius),
-            size = Size(radius*2, radius*2)
-        )
-        drawArc(
-            color = Color.Red,
-            startAngle = 198f,
-            sweepAngle = 72f,
-            useCenter = false,
-            style = Stroke(80f),
-            topLeft = Offset(shapeCenter.x-radius,shapeCenter.y-radius),
-            size = Size(radius*2, radius*2)
-        )
-        drawCircle(color = Color.Blue, center = handleCenter, radius = 60f)
+        {
+            shapeCenter = center
+            radius = size.minDimension / 2 + 100
+            val x = (shapeCenter.x + cos(toRadians(angle)) * radius).toFloat()
+            val y = (shapeCenter.y + sin(toRadians(angle)) * radius).toFloat()
+            handleCenter = Offset(x, y)
 
-    }
-    Image(
-        painter = painterResource(
-            id = getPic(angle)),
-        contentDescription = stringResource(id = R.string.dog_content_description),
-        contentScale = ContentScale.Fit,
-        modifier = imageModifier,
-    )
+            drawArc(
+                color = Color.Green,
+                startAngle = 270f,
+                sweepAngle = 72f,
+                useCenter = false,
+                style = Stroke(80f),
+                topLeft = Offset(shapeCenter.x - radius, shapeCenter.y - radius),
+                size = Size(radius * 2, radius * 2)
+            )
+            drawArc(
+                color = Color.Cyan,
+                startAngle = 342f,
+                sweepAngle = 72f,
+                useCenter = false,
+                style = Stroke(80f),
+                topLeft = Offset(shapeCenter.x - radius, shapeCenter.y - radius),
+                size = Size(radius * 2, radius * 2)
+            )
+            drawArc(
+                color = Color.Yellow,
+                startAngle = 54f,
+                sweepAngle = 72f,
+                useCenter = false,
+                style = Stroke(80f),
+                topLeft = Offset(shapeCenter.x - radius, shapeCenter.y - radius),
+                size = Size(radius * 2, radius * 2)
+            )
+            drawArc(
+                color = Color.Magenta,
+                startAngle = 126f,
+                sweepAngle = 72f,
+                useCenter = false,
+                style = Stroke(80f),
+                topLeft = Offset(shapeCenter.x - radius, shapeCenter.y - radius),
+                size = Size(radius * 2, radius * 2)
+            )
+            drawArc(
+                color = Color.Red,
+                startAngle = 198f,
+                sweepAngle = 72f,
+                useCenter = false,
+                style = Stroke(80f),
+                topLeft = Offset(shapeCenter.x - radius, shapeCenter.y - radius),
+                size = Size(radius * 2, radius * 2)
+            )
+            drawCircle(color = Color.Blue, center = handleCenter, radius = 60f)
+        }
+
+        Image(
+            painter = painterResource(
+                id = getPic(angle)
+            ),
+            contentDescription = stringResource(id = R.string.dog_content_description),
+            contentScale = ContentScale.Fit,
+            modifier = imageModifier,
+        )
+   // }
 }
 
 private fun getPic(angl: Double): Int {
+    Log.d("RRubi",angl.toString())
     when (angl) {
-        in 1.0..72.0 -> pic =  R.raw.pic2
-        in 72.1..144.0 -> pic =  R.raw.pic3
-        in 144.1..216.0 -> pic =  R.raw.pic4
-        in 216.1..288.0 -> pic =  R.raw.pic5
-        in 288.1..360.0 -> pic =  R.raw.pic1
+        in 270.1..342.0 -> pic =  R.raw.pic1
+        in 342.1..359.9 -> pic =  R.raw.pic2
+        in 0.0..54.0 -> pic =  R.raw.pic2
+        in 54.1..126.0 -> pic =  R.raw.pic3
+        in 126.1..198.0 -> pic =  R.raw.pic4
+        in 198.1..270.0 -> pic =  R.raw.pic5
         else -> print("none of the above")
     }
     return pic
@@ -177,8 +185,6 @@ private fun getRotationAngle(currentPosition: Offset, center: Offset): Double {
     if (angle < 0) {
         angle += 360.0
     }
-    Log.d("RRubi","angle: "+angle.toString())
-    //Log.d("RRubi", "width: "+width.toString()+" height: "+ height.toString()+"  rd: "+rd.toString()+"  dns: "+dns.toString())
     return angle
 }
 
